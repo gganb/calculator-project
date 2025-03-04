5b2e6bb22ad47e48bf2bc0962509634e0e948cc2
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-04 13:55:13 +0900

    Feat : 리펙터링 기본 생성자로 호출하도록 함. 무한루프 내에서 값을 계속 초기화해 기본생성자로변경함 연산을 수행하는 switch 문을 Calculator 클래스 내부로 숨김. 점선 길이 모두 동일하도록 수정 연산자 입력시 공백 제거하도록 수정 사칙연산자 이외의 문자가 들어오면 예외를 처리하도록 추가 저장 기록 메뉴 switch문으로 수행하도록 수정

--- src/com/example/calculator/lv2/App.java
+++ src/com/example/calculator/lv2/App.java
@@ -1,62 +1,76 @@
 package com.example.calculator.lv2;
 
+import com.example.calculator.CalculationHistory;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Calculator myCalculator = new Calculator(0, 0, ' ');
-        try {
-            // 양의 정수 입력받기
-            System.out.print("숫자 1 입력 : ");
-            int num1 = Integer.parseInt(scan.nextLine());
+        CalculationHistory calculationHistory = new CalculationHistory();   // 저장 클래스
+        Calculator myCalculator = new Calculator(); // Calculator 생성자 초기화
 
-            System.out.print("숫자 2 입력 : ");
-            int num2 = Integer.parseInt(scan.nextLine());
+        while (true) { // 사용자가 exit 입력 시 종료
+            try {
+                System.out.println("------- 계산하기 -------");
+                System.out.print("숫자 1 입력 : ");
+                int num1 = Integer.parseInt(scan.nextLine());
+                myCalculator.setNum1(num1);   // num1 값 설정
 
-            // 입력받은 값이 양의 정수가 아닐 때 예외 발생시키기
-            if (num1 < 0 || num2 < 0) {
-                throw new RuntimeException("양의 정수만 입력하세요");
+                System.out.print("연산자 입력  : ");
+                String str = scan.nextLine().trim();
+                char operate = str.charAt(0);
+                myCalculator.setOperator(operate);    // 연산자 설정
+
+                System.out.print("숫자 2 입력 : ");
+                int num2 = Integer.parseInt(scan.nextLine());
+                myCalculator.setNum2(num2);   // num2 값 설정
+
+
+                System.out.println(myCalculator.getCalculation());   //정답 출력
+                calculationHistory.saveHistory(myCalculator.getCalculation());  // 계산 결과 저장
+
+                System.out.println("-----------------------");
+                System.out.println("더 진행하시겠습니까? (exit or yes) : ");
+                String msg = scan.nextLine();
+
+                if (msg.equals("exit")) {   // 종료여부 묻기
+                    break;
+                } else if (msg.equals("yes")) {
+                    continue;
+                } else System.out.println("일치하지 않습니다"); //다시 계산하기로 돌아감
+
+            } catch (ArithmeticException zero) {    // 0으로 나눌 경우 예외 처리
+                System.out.println(zero.getMessage());
+
+            } catch (IllegalArgumentException I) {
+                System.out.println(I.getMessage());
+            } catch (RuntimeException e) {  // 양수 외의 숫자 입력 시 예외 처리
+                System.out.println(e.getMessage());
+            }
+        } //while종료
+
+        System.out.println("[ 저장 기록 메뉴 ]");
+        boolean setting = true;
+        while (setting) {       //저장 기록 메뉴
+            System.out.print(" (1) 불러오기 (2) 삭제하기 (3) 나가기 : ");
+            int askMsg = Integer.parseInt(scan.nextLine()); //번호 입력 받기
+            switch (askMsg) {
+                case 1:
+                    System.out.println("------- 계산 기록 -------");
+                    calculationHistory.printHistory();
+                    break;
+                case 2:
+                    calculationHistory.clearHistory();
+                    break;
+                case 3:
+                    System.out.println("종료합니다.");
+                    setting = false;
+                    break;
+                default:
+                    System.out.println("잘못된 선택입니다. 다시 시도해주세요.");
             }
 
-            // 사칙 연산 기호 입력 받기
-            System.out.print("연산자를 입력하세요 ( +, -, /, x ) : ");
-            String str = scan.nextLine();
-            char operator = str.charAt(0);
-
-            Number result = 0;
-            switch (operator) { //입력한 연산자에 따라 switch 문 동작
-                case '+':
-                    result = myCalculator.add(num1, num2);
-                    break;
-                case '-':
-                    result = myCalculator.subtract(num1, num2);
-                    break;
-                case 'x':
-                    result = myCalculator.multiply(num1, num2);
-                    break;
-                case '/':
-                    result = myCalculator.divide(num1, num2);
-                    break;
-                default:    // 사칙연산 외의 연산자가 들어왔을 때 처리
-                    System.out.println("계산할 수 없습니다.");
-            }
-            System.out.println("결과 : " + result);
-
-            System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료) : ");
-            String msg = scan.nextLine();
-
-        } catch (NumberFormatException e) {
-            System.out.println("Error !");
-            System.out.println("숫자만 입력해주세요.");
-        } catch (ArithmeticException a) {
-            System.out.println("Error !");
-            System.out.println(a.getMessage());
-        } catch (RuntimeException r) {
-            System.out.println("Error !");
-            System.out.println(r.getMessage());
-        }// end of catch
-
-        //   }// end of while
-    }
-}
+        }// while2 종료
+    }//main
+} //class
