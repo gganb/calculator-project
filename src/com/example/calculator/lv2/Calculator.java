ab3124e3658daacaa6b37b169ac73f312945e31b
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-04 13:58:42 +0900

    생성자를 기본 생성자로 변경함 연산자를 변경하는 setOperator에서 조건에 벗어나는 값 입력시 예외를 던지도록 수정 연산하는 switch문을 calculate 메서드를 추가해 캡슐화함. 결과를 출력하는 메서드 추가

--- src/com/example/calculator/lv2/Calculator.java
+++ src/com/example/calculator/lv2/Calculator.java
@@ -1,29 +1,70 @@
 package com.example.calculator.lv2;
 
-public class Calculator {
+public class Calculator {    //값 계산하는 클래스
     private char operator;
     private int num1, num2;
 
-    Calculator(int num1, int num2, char operator) {
-        this.operator = operator;
-        this.num1 = num1;
-        this.num2 = num2;
+
+    // 생성자 초기화
+    public Calculator() {
     }
 
-    public int add(int num1, int num2){
-        return num1 + num2;
+    // 숫자,연산자 getter & setter
+    public int getNum1() {
+        return num1;
     }
-    public int subtract(int num1, int num2){
-        return num1 - num2;
-    }
-    public int multiply(int num1, int num2){
-        return num1 * num2;
-    }
-    public double divide(int num1, int num2) {
-        if (num2 == 0) {
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");    // 예외 발생
+
+    public void setNum1(int num1) { // 양수 외의 값으로 변경할 수 없음
+        if (num1 < 0) {
+            throw new RuntimeException("양의 정수를 입력해주세요"); //예외 발생
         }
-        return num1 / (double) num2;
+        this.num1 = num1;   // 0을 포함하는 양수일 경우 값 변경
     }
 
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) { // 양수 외의 값으로 변경할 수 없음
+        if (num2 < 0) {
+            throw new RuntimeException("양의 정수를 입력해주세요"); //예외 발생
+        } else {
+            this.num2 = num2;  // 0을 포함하는 양수일 경우 값 변경
+        }
+    }
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public void setOperator(char operator) {    // 연산자 설정
+        if (!(operator == '+' || operator == '-' || operator == '/' || operator == 'x')) {
+            throw new IllegalArgumentException("알맞은 연산자를 입력해주세요.");
+        }
+        this.operator = operator;
+    }
+
+    //연산하기
+    public Number calculate() {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case 'x':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
+                return num1 / (double) num2;
+            default:
+                throw new IllegalArgumentException("알맞은 연산자가 아닙니다.");
+        }
+
+    }
+
+    public String getCalculation() {
+        return String.format("%d %c %d = %s", num1, operator, num2, calculate());
+    }
 }
