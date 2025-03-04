cb90880c4131369ff224fa6890853731e58fae31
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-04 19:21:34 +0900

    Feat : 연산자를 열거형 상수로 처리 사칙 연산 기호들을 상수로 처리한 뒤, 각 연산자에 맞는 람다식으로 초기화함 추상메서드를 받아와 연결하는 메서드 추가 연산자를 char형으로 얻는 메서드 구현 입력된 문자가 유효한 연산자인지 검사하는 메서드 구현

--- src/com/example/calculator/lv3/OperatorType.java
+++ src/com/example/calculator/lv3/OperatorType.java
@@ -1,30 +1,36 @@
 package com.example.calculator.lv3;
 
 
-public enum OperatorType2 {
+public enum OperatorType { // 사칙연산 기호들을 상수로 처리한 뒤 람다식을 이용해 초기화함
     PLUS('+', (a, b) -> a + b),
     MINUS('-', (a, b) -> a - b),
     TIMES('x', (a, b) -> a * b),
     DIVISION('/', (a, b) -> {
-        if (b == 0) {
+        if (b == 0) {   // 분모가 0 일 경우 예외 처리
             throw new ArithmeticException("0으로 나눌 수 없습니다.");
         }
         return a / b;
     });
 
     private final char operator;
+    private final Apply apply; // 함수 인터페이스
 
-    OperatorType2(char operator, Apply apply) {
+    OperatorType(char operator, Apply apply) { // 연산 기호, 람다식으로 초기화
         this.operator = operator;
+        this.apply = apply;
     }
 
-    public char getOperator() {
+    public double calculate(int a, int b) { //추상 메서드와 연결
+        return this.apply.apply(a, b);
+    }
+
+    public char getOperator() { // (char) 연산자 얻기
         return operator;
     }
 
-    public static OperatorType2 inspectOperator(char operator){
-        for(OperatorType2 type : OperatorType2.values()){
-            if(type.getOperator() == operator)
+    public static OperatorType inspectOperator(char operator) { // 입력된 문자가 유효한 연산자인지 검사
+        for (OperatorType type : OperatorType.values()) {
+            if (type.getOperator() == operator)
                 return type;
         }
         throw new IllegalArgumentException("잘못된 연산자 입니다.");
