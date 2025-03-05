69f76643669b90e4e058bcd34afabedcc9cf8323
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-05 17:55:05 +0900

    Feat : enum 수정 기존엔 enum에서 각 상수가 람다식을 이용해 계산을 구현했지만 튜터님의 답을 듣고 추상메서드를 구현하는 방식으로 변경 추상메서드 apply 구현 함수형 인터페이스 삭제

--- src/com/example/calculator/lv3/OperatorType.java
+++ src/com/example/calculator/lv3/OperatorType.java
@@ -1,29 +1,54 @@
 package com.example.calculator.lv3;
 
-
-public enum OperatorType { // 사칙연산 기호들을 상수로 처리한 뒤 람다식을 이용해 초기화함
-    PLUS('+'),
-    MINUS('-'),
-    TIMES('x'),
-    DIVISION('/');
+//연산자 enum
+public enum OperatorType {
+    PLUS('+') {
+        @Override
+        public double apply(double a, double b) {
+            return a + b;
+        }
+    },
+    MINUS('-') {
+        @Override
+        public double apply(double a, double b) {
+            return a - b;
+        }
+    },
+    TIMES('x') {
+        @Override
+        public double apply(double a, double b) {
+            return a * b;
+        }
+    },
+    DIVISION('/') {
+        @Override
+        public double apply(double a, double b) {
+            if (b == 0) {
+                throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            }
+            return a / b;
+        }
+    };
 
     private final char operator;
-    // private final Apply apply; // 함수 인터페이스
 
-    OperatorType(char operator) { // 연산 기호
+    OperatorType(char operator) { // 생성자
         this.operator = operator;
     }
 
-
-    public char getOperator() { // (char) 연산자 얻기
+    public char getOperatorType() { // (char) 연산자 얻기
         return operator;
     }
 
-    public static OperatorType inspectOperator(char operator) { // 입력된 문자가 유효한 연산자인지 검사
+    public abstract double apply(double a, double b);   // 추상메서드
+
+    public static OperatorType inspectOperator(char operator) { // 입력된 문자가 유효한 연산자인지 검사, 연산자 반환
         for (OperatorType type : OperatorType.values()) {
-            if (type.getOperator() == operator)
+            if (type.getOperatorType() == operator)
                 return type;
         }
         throw new IllegalArgumentException("잘못된 연산자 입니다.");
     }
+
+
 }
