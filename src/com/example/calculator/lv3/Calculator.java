cfc8a82bed3ec8dc69517890e7fc9da21f31c587
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-05 17:58:57 +0900

    Feat : 제네릭 이용 제네릭 구현해 Number의 자손까지 타입을 받을 수 있도록 범위를 제한해 변경 계산을 해당 클래스에서 구현했으나 OperatorType에 계산기능을 넘겨 기존의 계산하는 메서드에서는 값을 전달만 하도록 수정 계산된 실수 값을 소숫점 둘째자리까지만 출력하도록 수정 계산 결과를 출력하는 메서드에서 출력방식 수정

--- src/com/example/calculator/lv3/Calculator.java
+++ src/com/example/calculator/lv3/Calculator.java
@@ -1,71 +1,51 @@
 package com.example.calculator.lv3;
 
+public class Calculator<T extends Number> {    //값 계산하는 클래스
+    private T num1;
+    private T num2;
+    private OperatorType operator;
 
-import static com.example.calculator.lv3.OperatorType.PLUS;
-
-public class Calculator {    //값 계산하는 클래스
-    private int num1;
-    private int num2;
-    private char operator;
-    // private final Apply apply;
-
-
-    // 생성자 초기화
+    // 기본 생성자
     public Calculator() {
-
     }
 
     // 숫자,연산자 getter & setter
-    public int getNum1() {
+    public T getNum1() {
         return num1;
     }
 
-    public void setNum1(int num1) {
+    public void setNum1(T num1) {
         this.num1 = num1;
     }
 
-    public int getNum2() {
+    public T getNum2() {
         return num2;
     }
 
-    public void setNum2(int num2) {
+    public void setNum2(T num2) {
         this.num2 = num2;
     }
 
     public void setOperator(char operator) {    // 연산자 설정
-        OperatorType validOperator = OperatorType.inspectOperator(operator);
-        this.operator = validOperator.getOperator();
+        this.operator = OperatorType.inspectOperator(operator);
     }
 
-    public char getOperator() {
-        return this.operator;
+    public OperatorType getOperator() {
+        return operator;
     }
-    //연산하기
-//    public T calculate(T a, T b) { //추상 메서드와 연결
-//        return apply.apply(a, b);
-//    }
-
-    public double calculate(int num1, int num2) {
-        OperatorType operatorType = OperatorType.inspectOperator(operator);
-        switch (operatorType) {
-            case PLUS:
-                return num1 + num2;
-            case MINUS:
-                return num1 - num2;
-            case TIMES:
-                return num1 * num2;
-            case DIVISION:
-                if (num2 == 0) {
-                    throw new ArithmeticException("0으로 나눌 수 없습니다");
-                }
-                return (double) num1 / num2;
-            default:
-                throw new IllegalArgumentException("알맞은 연산자가 아닙니다.");
+//enum 내의 각 연산자로 값 전달하는 메서드
+    public Number calculate() {
+        double result = operator.apply(num1.doubleValue(), num2.doubleValue()); //double 형태로 전달
+        result = Double.parseDouble(String.format("%.2f", result)); // 소수점 둘째자리까지 출력
+        if (num1 instanceof Integer && num2 instanceof Integer) {
+            return Integer.valueOf((int) result);    // Integer 반환
+        } else {
+            return result;  // Double 반환
         }
     }
 
     public String getCalculation() {
-        return String.format("%d %c %d = %s", num1, operator, num2, calculate(num1, num2));
+        return String.format("%s %c %s = %s", num1, operator.getOperatorType(), num2, calculate());
     }
 
 
