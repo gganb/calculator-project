f5de914e5d1a375474ae832140bd912a8144bd69
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-05 14:47:38 +0900

    Feat : 기능 수정 enum의 연산자에 람다식을 함께 작성했으나 제네릭으로 변경하기 어려워 char값만가지고 있도록 수정

--- src/com/example/calculator/lv3/OperatorType.java
+++ src/com/example/calculator/lv3/OperatorType.java
@@ -2,27 +2,18 @@ package com.example.calculator.lv3;
 
 
 public enum OperatorType { // 사칙연산 기호들을 상수로 처리한 뒤 람다식을 이용해 초기화함
-    PLUS('+', (a, b) -> a + b),
-    MINUS('-', (a, b) -> a - b),
-    TIMES('x', (a, b) -> a * b),
-    DIVISION('/', (a, b) -> {
-        if (b == 0) {   // 분모가 0 일 경우 예외 처리
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
-        }
-        return a / b;
-    });
+    PLUS('+'),
+    MINUS('-'),
+    TIMES('x'),
+    DIVISION('/');
 
     private final char operator;
-    private final Apply apply; // 함수 인터페이스
+    // private final Apply apply; // 함수 인터페이스
 
-    OperatorType(char operator, Apply apply) { // 연산 기호, 람다식으로 초기화
+    OperatorType(char operator) { // 연산 기호
         this.operator = operator;
-        this.apply = apply;
     }
 
-    public double calculate(int a, int b) { //추상 메서드와 연결
-        return this.apply.apply(a, b);
-    }
 
     public char getOperator() { // (char) 연산자 얻기
         return operator;
