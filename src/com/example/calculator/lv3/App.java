f4f00ae334418583c54479cd7a90d2e29e564c37
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-05 22:13:06 +0900

    Feat : 위치 수정 및 기능 추가 try의 위치를 변경해 잘못된 범위 지정으로 인해 예외가 발생하면 무한루프가 멈추고 에러가 뜨는 점을 고침. 이후 예외가 발생해도 오류를 알려주고 연산을 진행함 사용자가 값을 입력했을 때 저장되었던 결과값보다 큰 값들을 출력하는 기능을 추가함 저장 메뉴에 case를 추가해 해당 기능을 선택할 수 있도록 설계했으며 입력한 값보다 큰 결과값이 없다면 메시지를 출력한 뒤 반복하는 동작을 함. 값이 있다면 줄바꿈을 하며 내용이 출력됨

--- src/com/example/calculator/lv3/App.java
+++ src/com/example/calculator/lv3/App.java
@@ -1,5 +1,6 @@
 package com.example.calculator.lv3;
 
+import java.util.*;
 import java.util.Scanner;
 
 public class App {
@@ -13,26 +14,26 @@ public class App {
             System.out.print("숫자 1 입력 : ");
             double num1 = Double.parseDouble(scan.nextLine());
             myCalculator.setNum1(num1);   // num1 값 설정
-
-            System.out.print("연산자 입력  : ");
-            String str = scan.nextLine().trim();
-            char operate = str.charAt(0);
-            myCalculator.setOperator(operate);    // 연산자 설정
-
-            System.out.print("숫자 2 입력 : ");
-            double num2 = Double.parseDouble(scan.nextLine());
-            myCalculator.setNum2(num2);   // num2 값 설정
-            // System.out.println("숫2 입력 완료");
-            if (operate == '/' && num2 == 0) {
-                System.out.println("0으로 나눌 수 없습니다. 다시 입력해 주세요.");
-                continue;
-            }
             try {
-                // System.out.println("정답시작");
+                System.out.print("연산자 입력  : ");
+                String str = scan.nextLine().trim();
+                char operate = str.charAt(0);
+                myCalculator.setOperator(operate);    // 연산자 설정
+
+                System.out.print("숫자 2 입력 : ");
+                double num2 = Double.parseDouble(scan.nextLine());
+                myCalculator.setNum2(num2);   // num2 값 설정
+
+                if (operate == '/' && num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다. 다시 입력해 주세요.");
+                    continue;
+                }
+
                 System.out.println(myCalculator.getCalculation());   //정답 출력
                 calculationHistory.saveHistory(myCalculator.getCalculation());  // 계산 결과 저장
             } catch (ArithmeticException zero) {    // 0으로 나눌 경우 예외 처리
                 System.out.println(zero.getMessage());
+                continue;
             } catch (IllegalArgumentException I) {
                 System.out.println(I.getMessage());
                 continue;
@@ -52,13 +53,14 @@ public class App {
         scan.close();   // scanner 닫기
     } // main
 
+
     private static void showHistoryMenu(Scanner scan, CalculationHistory calculationHistory) {
 
         System.out.println("[ 저장 기록 메뉴 ]");
-        boolean setting = true;
-        while (setting) {       //저장 기록 메뉴
-            System.out.print(" (1) 불러오기 (2) 삭제하기 (3) 나가기 : ");
-            int askMsg = Integer.parseInt(scan.nextLine().trim()); //번호 입력 받기
+        //  boolean setting = true;
+        while (true) {       //저장 기록 메뉴
+            System.out.print(" (1) 불러오기 (2) 삭제하기 (3) 나가기 (4) 값 비교하기: ");
+            int askMsg = Integer.parseInt(scan.nextLine()); //번호 입력 받기
             switch (askMsg) {
                 case 1:
                     System.out.println("------- 계산 기록 -------");
@@ -69,11 +71,20 @@ public class App {
                     break;
                 case 3:
                     System.out.println("종료합니다.");
-                    setting = false;
+                    return;
+                case 4:
+                    System.out.println("----- 입력값 보다 큰 결과 출력하기 -----");
+                    System.out.print("입력 : ");
+                    double num3 = Double.parseDouble(scan.nextLine());
+                    List<String> result = calculationHistory.findToResult(num3);
+                    if (result.isEmpty()) { // 큰 값이 없다면 result에 아무 값도 존재하지않음.
+                        System.out.println("큰 값이 없습니다");
+                    } else result.forEach(System.out::println); // 존재하다면 출력
                     break;
                 default:
                     System.out.println("잘못된 선택입니다. 다시 시도해주세요.");
             }
         }
     }
+
 } //class
