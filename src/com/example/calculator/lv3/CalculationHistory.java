cbd607de65ddd6232a0c5d91b2377951a85a7042
박한비 <146406303+gganb@users.noreply.github.com>
2025-03-05 22:20:13 +0900

    Feat : 기능 추가 및 삭제 기능 수정 결과 값을 List에 String 타입으로 저장했기에 =을 기준으로 잘라서 문자열 배열에 저장하도록 만듦 두 값으로 분리되었기에 인덱스1 값(결과값)을 double형태로 변환했을 때 입력받은 값 보다 크다면 filter()를 통해 추출되도록 작성 이후 최종 연산으로 List<String> 형태로 리턴하도록 작성함. 삭제 기능에서 중복된 메서드로 인해 두 값씩 삭제되던 오류를 발견해 중복된 메서드를 삭제해 해결함

--- src/com/example/calculator/lv3/CalculationHistory.java
+++ src/com/example/calculator/lv3/CalculationHistory.java
@@ -1,10 +1,15 @@
 package com.example.calculator.lv3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class CalculationHistory {
-    private List<String> calculatorHistory = new ArrayList<>();
+    private final List<String> calculatorHistory = new ArrayList<>();
 
     public void saveHistory(String history) {
         calculatorHistory.add(history);
@@ -24,10 +29,22 @@ public class CalculationHistory {
         if (calculatorHistory.isEmpty()) {  //기록이 비워져있을 때
             System.out.println("계산 기록이 없습니다.");
         } else {
-            String removed = calculatorHistory.remove(calculatorHistory.size() - 1);  //삭제할 기록 출력
-            System.out.println(removed);
-            System.out.println("삭제되었습니다.");
-            calculatorHistory.remove(calculatorHistory.size() - 1);   //마지막 인덱스 삭제
+            System.out.println(calculatorHistory.remove(calculatorHistory.size() - 1));//삭제할 기록 출력
+            System.out.println("삭제되었습니다."); //마지막 인덱스 삭제
         }
+    }
+
+    public List<String> findToResult(double result) {   //입력값 보다 큰 결과 값 찾기
+        return calculatorHistory.stream()
+                .filter(str -> {
+                    try {
+                        String[] resultStr = str.split("="); // "="을 기준으로 문자열 자르기, "="을 기준으로 뒤의 값은 결과값
+                        return resultStr.length == 2 && Double.parseDouble(resultStr[1]) > result; // 배열 길이가 2이고 결과값을 가진 인덱스의 실수로 변환한 값이 입력받은 값 보다 크다면 필터링해서 str에 저장함
+                    } catch (NumberFormatException e) { //없다면 필터링하지 않고 false 리턴
+                        System.out.println("없습니다");
+                        return false;
+                    }
+                })   // 중간 연산. 반복 수행해 원하는 값 출력
+                .collect(Collectors.toList()); // 최종 출력.
     }
 }
